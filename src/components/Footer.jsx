c625f5e45d55961ac26118cc563e1c34a2905967
Samuel Ogwuche <samogwuche98@gmail.com>
2025-11-08 18:57:33 +0100

    add dynamic year function to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import assets from "../assets/assets";
 
 const Footer = ({ theme }) => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <div className="bg-slate-50 dark:bg-gray-900 pt-10 sm:pt-10 mt-20 sm:mt-40 px-4 sm:px-10 lg:px-24 xl:px-40">
       {/* footer top */}
@@ -64,7 +66,7 @@ const Footer = ({ theme }) => {
 
       {/* footer bottom */}
       <div className="pb-6 text-sm text-gray-500 flex justify-center sm:justify-between gap-4 flex-wrap">
-        <p>Copyright 2025 @ agency.io - All Right Reserved.</p>
+        <p>Copyright {currentYear} @ agency.io - All Right Reserved.</p>
         <div className="flex items-center justify-between gap-4">
           <img src={assets.facebook_icon} alt="" />
           <img src={assets.twitter_icon} alt="" />
